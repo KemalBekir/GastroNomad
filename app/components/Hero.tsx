ad8b012544556bb2fd8182f6212075c3b086990e
Kemal <outsider.pld@gmail.com>
2023-08-04 19:00:42 +0100

    Created - place holder for mock data

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="h-screen">
+    <div className="h-screen w-screen">
       {/* Gradient background */}
       <div className="w-full h-full bg-gradient-to-b from-white to-gray-300 relative">
         <div className="relative w-full h-full">
@@ -13,17 +13,17 @@ const Hero = () => {
             <img src="logo.png" alt="" />
           </div>
           {/* Background Image */}
-          <div className="absolute top-1/3 left-1/3 z-30 w-[90%] ">
-            <div className="w-full flex justify-between flex-row flex-nowrap items-center">
-              <div className="flex flex-col w-[70%]">
-                <h1 className=" text-white text-6xl uppercase tracking-widest mt-8">
+          <div className="absolute top-1/3 left-1/3 z-30 w-[90%] max-w-screen-lg px-4">
+            <div className="w-full flex flex-row flex-nowrap items-center">
+              <div className="flex flex-col w-[50%]">
+                <h1 className="text-white text-6xl uppercase tracking-widest mt-8">
                   Gastro Nomad
                 </h1>
                 <p className="mt-10 text-3xl text-gray-400 w-3/4">
                   Culinary adventures and cultural exploration worldwide
                 </p>
               </div>
-              <div className="flex flex-col absolute right-2 w-[30%] justify-between">
+              <div className="flex flex-col absolute right-2 w-[30%]">
                 <Link href="/facebook" className="text-white my-[5px]">
                   <Image
                     src="/facebook.png"
@@ -51,6 +51,7 @@ const Hero = () => {
               </div>
             </div>
           </div>
+
           <div className=" h-full w-full z-20 opacity-100">
             <img
               src="/plate-nobg.png"
---
+++ app/utils/mockData.ts
@@ -0,0 +1,28 @@
+// mockData.js
+const blogPosts = [
+    {
+      id: 1,
+      title: "Exploring Exotic Cuisines",
+      image: "https://example.com/images/post1.jpg",
+      description: "Embark on a culinary journey to discover the world's most exotic cuisines.",
+      likes: 156,
+    },
+    {
+      id: 2,
+      title: "A Taste of Culture",
+      image: "https://example.com/images/post2.jpg",
+      description: "Delve into diverse cultures and their rich culinary traditions.",
+      likes: 235,
+    },
+    {
+      id: 3,
+      title: "Savoring Sweet Delights",
+      image: "https://example.com/images/post3.jpg",
+      description: "Indulge in a delectable collection of desserts from around the globe.",
+      likes: 321,
+    },
+    // Add more blog posts as needed
+  ];
+  
+  export default blogPosts;
+  
