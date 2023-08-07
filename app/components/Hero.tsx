9ce6997a60985ee11e4e4573105aacda5fadb2a4
Kemal <outsider.pld@gmail.com>
2023-08-07 17:02:15 +0100

    Changes to the design - first minor change to hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,71 +1,25 @@
 // pages/index.js
-
 import Image from "next/image";
 import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="h-screen w-screen">
-      {/* Gradient background */}
-      <div className="w-full h-full bg-gradient-to-b from-white to-gray-300 relative">
-        <div className="relative w-full h-full">
-          <div className="top-1 left-0 w-[150px] h-[150px] ml-3">
-            <img src="logo.png" alt="" />
-          </div>
-          {/* Background Image */}
-          <div className="absolute top-1/3 left-1/3 z-30 w-[90%] max-w-screen-lg px-4">
-            <div className="w-full flex flex-row flex-nowrap items-center">
-              <div className="flex flex-col w-[50%]">
-                <h1 className="text-white text-6xl uppercase tracking-widest mt-8">
-                  Gastro Nomad
-                </h1>
-                <p className="mt-10 text-3xl text-gray-400 w-3/4">
-                  Culinary adventures and cultural exploration worldwide
-                </p>
-              </div>
-              <div className="flex flex-col absolute right-2 w-[30%]">
-                <Link href="/facebook" className="text-white my-[5px]">
-                  <Image
-                    src="/facebook.png"
-                    alt="Facebook"
-                    width={32}
-                    height={32}
-                  />
-                </Link>
-                <Link href="/instagram" className="text-white my-[5px]">
-                  <Image
-                    src="/instagram.png"
-                    alt="Instagram"
-                    width={32}
-                    height={32}
-                  />
-                </Link>
-                <Link href="/twitter" className="text-white my-[5px]">
-                  <Image
-                    src="/twitter.png"
-                    alt="Twitter"
-                    width={32}
-                    height={32}
-                  />
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          <div className=" h-full w-full z-20 opacity-100">
-            <img
-              src="/plate-nobg.png"
-              alt=""
-              className="absolute  bottom-1/4 -left-48 drop-shadow-2xlg  z-20"
-            />
-          </div>
-          {/* Image overlay */}
-          <div className="absolute top-0 right-0 h-4/5 w-[90%] bg-[#1c3039] z-10">
-            <div className="bg-[url('/wine-glass.jpg')] bg-cover bg-top bg-no-repeat w-full h-full opacity-10"></div>
-          </div>
-        </div>
+    <section className="relative h-screen flex flex-col items-center justify-center text-center text-white py-0 px-3">
+      <div className="video-docker absolute top-0 left-0 w-full h-full overflow-hidden">
+        <video
+          className="min-w-full min-h-full absolute object-cover"
+          src="/pexelVideo.mp4"
+          typeof="video/mp4"
+          autoPlay
+          muted
+          loop
+        ></video>
       </div>
-    </div>
+      <div className="absolute video-content space-y-2">
+        <h1 className="font-light text-6xl z-10">Welcome</h1>
+        <h3 className="font-light text-3xl z-10">Gastro Nomad</h3>
+      </div>
+    </section>
   );
 };
 
--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -15,26 +15,27 @@ const Posts: React.FC = () => {
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 mt-10 mx-2">
       {blogPosts.map((post: BlogPost) => (
-        <Link key={post.id} href={`/posts/${post.id}`}>
-          <a className="block cursor-pointer">
-            {" "}
-            {/* Use a block-level element and cursor-pointer class for proper link styling */}
-            <div className="bg-[#d5d8de] p-4 rounded-lg shadow-md">
-              <h2 className="text-center text-[#1c3039] text-2xl font-bold mb-2">
-                {post.title}
-              </h2>
-              <img
-                src={post.image}
-                alt={post.title}
-                className="mx-auto rounded-lg mb-2 h-44"
-              />
-              <p className="text-[#1c3039]">{post.description}</p>
-              <div className="flex justify-end items-center mt-4">
-                <span className="text-gray-500 mr-1">Likes:</span>
-                <span className="text-indigo-600 font-bold">{post.likes}</span>
-              </div>
+        <Link
+          key={post.id}
+          href={`/posts/${post.id}`}
+          className="block cursor-pointer"
+        >
+          {/* Use a block-level element and cursor-pointer class for proper link styling */}
+          <div className="bg-[#d5d8de] p-4 rounded-lg shadow-md">
+            <h2 className="text-center text-[#1c3039] text-2xl font-bold mb-2">
+              {post.title}
+            </h2>
+            <img
+              src={post.image}
+              alt={post.title}
+              className="mx-auto rounded-lg mb-2 h-44"
+            />
+            <p className="text-[#1c3039]">{post.description}</p>
+            <div className="flex justify-end items-center mt-4">
+              <span className="text-gray-500 mr-1">Likes:</span>
+              <span className="text-indigo-600 font-bold">{post.likes}</span>
             </div>
-          </a>
+          </div>
         </Link>
       ))}
     </div>
